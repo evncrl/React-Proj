ad1cfeb08e234e89b85163644bf7342c49caa20e
evncrl <carlevan.piad05@gmail.com>
2025-08-27 23:19:23 +0800

    Delete post daw

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,3 +1,4 @@
+
 const Post = require('../models/post');
 const slugify = require('slugify');
 
@@ -20,4 +21,47 @@ exports.create = async (req, res) => {
     if (post)
         return res.status(200).json(post)
     return res.status(400).json({ error: "post create error" })
+};
+
+exports.list = async (req, res) => {
+    const posts = await Post.find({})
+        .limit(10)
+        .sort({ createdAt: 'desc' })
+        .exec();
+
+    if (posts)
+        return res.status(200).json(posts)
+    return res.status(400).json({ error: "list posts error" })
+};
+
+exports.read = async (req, res) => {
+    console.log(req.params.slug)
+    // const slug = req.params.slug
+    const { slug } = req.params;
+    console.log(slug)
+    const post = await Post.findOne({ slug }).exec()
+    if (!post)
+        return res.status(400).json({ error: "post error" })
+    return res.status(200).json(post)
+}; 
+
+exports.update = async (req, res) => {
+    const { slug } = req.params;
+    const { title, content, user } = req.body;
+    const newSlug = slugify(title);
+    const post = await Post.findOneAndUpdate({ slug }, { title, content, user, slug:newSlug }, { new: true })
+    if (!post)
+        return res.status(400).json({ error: "udpate post error" })
+    return res.status(200).json(post)
+
+    
+};
+
+exports.remove = async (req, res) => {
+    // console.log(req.pramas.slug)
+    const { slug } = req.params;
+    const post = await Post.deleteOne({ slug })
+    if (!post)
+        return res.status(400).json({ error: "delete post error" })
+    return res.status(200).json({ message: "post deleted"} )
 };
